06d594590f0279d5e4bd9b6dd81b0caacddaaf60
kedzilla <xavierkedzi@gmail.com>
2017-03-20 22:56:30 -0400

    Confidence of rushhour on occupancy levels

--- train_occupancy_prediction.py
+++ train_occupancy_prediction.py
@@ -1,4 +1,5 @@
 from algos.pre_processing import filter_duplicates, filter_erroneous
+from algos.data_transformation import calculateRushHourConfidence
 from domain.occupancy import Occupancy
 from domain.station import Station
 from utils.file_utils import parse_csv_file_to_list, parse_json_file_to_list
@@ -59,40 +60,7 @@ def analyzeDay(daily_results):
     occupency_per_hour = daily_results.pivot(['hour', 'occupancy']).order_by('hour')
     # occupency_per_hour.print_table(max_rows=2000, max_columns=15)
 
-    calculateOccupancyStatistics(daily_results)
-
-
-def calculateOccupancyStatistics(occupancies):
-    morning_rushhour_start = 7
-    morning_rushhour_end = 9
-    evening_rushhour_start = 16
-    evening_rushhour_end = 20
-
-    numberOfEntries = len(occupancies.rows)
-
-    # Calculate % of HIGH level during rushhour period
-    high_occ = occupancies.where(lambda row: 'HIGH' == row['occupancy'])
-
-    entries_in_rushhour = high_occ.where(
-        lambda row: morning_rushhour_start <= row['date'].hour <= morning_rushhour_end or evening_rushhour_start <= row[
-            'date'].hour <= evening_rushhour_end)
-
-    print(len(entries_in_rushhour) / len(high_occ))
-
-    avg_occ = occupancies.where(lambda row: 'MEDIUM' == row['occupancy'])
-    low_occ = occupancies.where(lambda row: 'LOW' == row['occupancy'])
-
-    # Print graph of LOW occ / hour
-    print("LOW OCCUPANCY")
-    low_occ.bins('hour', 23, 0, 23).print_bars('hour', width=80)
-
-    # Print graph of MEDIUM occ / hour
-    print("MEDIUM OCCUPANCY")
-    avg_occ.bins('hour', 23, 0, 23).print_bars('hour', width=80)
-
-    # Print graph of HIGH occ / hour
-    print("HIGH OCCUPANCY")
-    high_occ.bins('hour', 23, 0, 23).print_bars('hour', width=80)
+    calculateRushHourConfidence(daily_results)
 
 
 if __name__ == "__main__":
